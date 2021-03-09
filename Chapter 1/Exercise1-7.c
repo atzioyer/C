ebdaa84831a2df93aa0b5f850b7dda2ffaf3c025
atzioyer <78566913+atzioyer@users.noreply.github.com>
2021-03-08 18:57:22 -0600

    Add files via upload

---
+++ Chapter 1/Exercise1-7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+ int main()
+ {
+  printf("\n EOF: %d\n", EOF );
+  printf("\n");
+
+  return 0;
+ }
+
+
+ /*basado en la solucion de esta pagina web
+ http://hitmontop-ejerciciosresueltos.blogspot.com/2011/01/kernighanritchie17.html*/
