d207c9631e31a1c52d46885fa535d8f0ed1377c6
atzioyer <78566913+atzioyer@users.noreply.github.com>
2021-03-08 18:21:09 -0600

    Add files via upload

---
+++ Chapter 1/Exercise 1-6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+ main()
+
+ {
+
+  int c;
+  int variable;
+
+  printf("\n ( c = getchar() == EOF)");
+  printf("\n EOF inclusive\n");
+
+
+
+  printf("%d\n", variable);
+
+ }
+
+
+/*basado en la solucion de esta pagina web
+http://hitmontop-ejerciciosresueltos.blogspot.com/2011/01/kernighanritchie16.html*/
